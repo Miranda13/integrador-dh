1f8410c9d0168150f2f3f594589af8d6fe0b643f
Jhonathan Antonio Colque <jantoniocolque@outlook.com>
2021-10-28 13:15:56 -0300

    calendar and location with view mobile and desktop

--- front-nido/src/components/Location.jsx
+++ front-nido/src/components/Location.jsx
@@ -1,61 +1,36 @@
 import React, { Fragment } from "react";
 import './styles/Location.css';
+import { useState, useEffect } from 'react';
 
-
-function Location() {
+function Location({ zIndexCalendar }) {
+    const [showList, setShowList] = useState(false);
+    const handleListLocation = (e) => {
+        setShowList(!showList);
+        zIndexCalendar();
+    }
+    const handleSelectLocation = (e) => {
+        const title_location = document.querySelector(".container-location__title");
+        title_location.innerHTML = "" + e.target.childNodes[1]?.textContent + ", " + e.target.childNodes[3]?.textContent;
+        title_location.style.color = "var(--dark-color)"
+    }
+    useEffect(() => {
+        const list = document.querySelector(".container-location__list");
+        if (showList) {
+            list.classList.remove("hideItem");
+        } else {
+            list.classList.add("hideItem");
+        }
+    }, [showList])
     return (
-        <div className="container-location">
-            <h2 className="container-location__title">¿A dónde vamos?</h2>
-            <ul className="container-location__list">
-                <li className="container-location__list__item"><a className="container-location__list__item__a" href="#"><strong>San Carlos de Bariloche</strong></a><br />Argentina</li>
-                <li className="container-location__list__item"><a className="container-location__list__item__a" href="#"><strong>Buenos Aires</strong></a><br />Argentina</li>
-                <li className="container-location__list__item"><a className="container-location__list__item__a" href="#"><strong>Mendoza</strong></a><br />Argentina</li>
-                <li className="container-location__list__item"><a className="container-location__list__item__a" href="#"><strong>Córdoba</strong></a><br />Argentina</li>
+        <div className="container-location" onClick={handleListLocation}>
+            <h2 className="container-location__title" >¿A dónde vamos?</h2>
+            <ul className="container-location__list hideItem">
+                <li className="container-location__list__item" onClick={handleSelectLocation}><i className="fas fa-map-marker-alt"></i><strong>San Carlos de Bariloche</strong><br />Argentina</li>
+                <li className="container-location__list__item" onClick={handleSelectLocation}><i className="fas fa-map-marker-alt"></i><strong>Buenos Aires</strong><br />Argentina</li>
+                <li className="container-location__list__item" onClick={handleSelectLocation}><i className="fas fa-map-marker-alt"></i><strong>Mendoza</strong><br />Argentina</li>
+                <li className="container-location__list__item" onClick={handleSelectLocation}><i className="fas fa-map-marker-alt"></i><strong>Córdoba</strong><br />Argentina</li>
             </ul>
-        </div>
-
-        // <Fragment>
-        //     <div className="contendor">
-        //         <form action="">
-        //             <div className="selectbox">
-        //                 <div className="select active" id="select">
-        //                     <div className="contenido-select">
-        //                         <h1 className="ciudad">¿A dónde vamos?</h1>
-        //                         <p className="pais"></p>
-        //                     </div>
-        //                     <i class="fas fa-map-marker-alt"></i>
-        //                 </div>
-        //                 <div className="opciones active" id="opciones">
-        //                     <a href="#" class="opcion">
-        //                         <div className="contenido-opcion">
-        //                             <i class="fas fa-map-marker-alt"></i>
-        //                             <div className="textos">
-        //                                 <div className="ciudad">
-        //                                    <h2>San Carlos de Bariloche</h2> 
-        //                                    <p className="descripcion">Argentina</p>
-        //                                 </div>
-        //                             </div>
-        //                         </div>
-        //                     </a>
-        //                     <a href="#" class="opcion">
-        //                         <div className="contenido-opcion">
-        //                             <i class="fas fa-map-marker-alt"></i>
-        //                             <div className="textos">
-        //                                 <div className="ciudad">
-        //                                    <h2>Mendoza</h2> 
-        //                                    <p className="descripcion">Argentina</p>
-        //                                 </div>
-        //                             </div>
-        //                         </div>
-        //                     </a>
-        //                 </div>
-        //             </div>
-        //         </form>
-        //     </div>
-        // </Fragment>
-
-
-
+        </div >
     )
 }
 
