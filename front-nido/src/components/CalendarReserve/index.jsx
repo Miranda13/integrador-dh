d43b76a4f6e74ad2bb386cd626a0d671d9a90e9c
Jhonathan Antonio Colque <jantoniocolque@outlook.com>
2021-11-03 11:07:24 -0300

    component-calendar

---
+++ front-nido/src/components/CalendarReserve/index.jsx
@@ -0,0 +1,78 @@
+import { useState, useEffect } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { registerLocale, setDefaultLocale } from "react-datepicker";
+import es from 'date-fns/locale/es';
+export default function CalendarReserve() {
+    registerLocale('es', es)
+    const [startDate, setStartDate] = useState();
+    const [endDate, setEndDate] = useState();
+    function editNamesDaysWeek() {
+        const namesDaysWeek = document.querySelectorAll(".react-datepicker__day-name");
+        namesDaysWeek.forEach(name => {
+            name.innerHTML = name.textContent.substring(0, 1).toUpperCase();
+        })
+    }
+    useEffect(() => {
+        editNamesDaysWeek();
+    }, [startDate, endDate])
+    return (
+        <div>
+            <DatePicker
+                inline
+                locale="es"
+                minDate={new Date()}
+                maxDate={new Date()}
+                renderCustomHeader={({
+                    monthDate,
+                    customHeaderCount,
+                    decreaseMonth,
+                    increaseMonth,
+                }) => (
+                    <div>
+                        <button
+                            aria-label="Previous Month"
+                            className={
+                                "react-datepicker__navigation react-datepicker__navigation--previous"
+                            }
+                            style={customHeaderCount === 1 ? { visibility: "hidden" } : null}
+                            onClick={decreaseMonth}
+                        >
+                            <span
+                                className={
+                                    "react-datepicker__navigation-icon react-datepicker__navigation-icon--previous"
+                                }
+                            >
+                                {"<"}
+                            </span>
+                        </button>
+                        <span className="react-datepicker__current-month">
+                            {monthDate.toLocaleString("es-ES", {
+                                month: "long",
+                            }).substring(0, 1).toUpperCase() + monthDate.toLocaleString("es-ES", { month: "long" }).slice(1)}
+                        </span>
+                        <button
+                            aria-label="Next Month"
+                            className={
+                                "react-datepicker__navigation react-datepicker__navigation--next"
+                            }
+                            style={customHeaderCount === 0 ? { visibility: "hidden" } : null}
+                            onClick={increaseMonth}
+                        >
+                            <span
+                                className={
+                                    "react-datepicker__navigation-icon react-datepicker__navigation-icon--next"
+                                }
+                            >
+                                {">"}
+                            </span>
+                        </button>
+                    </div>
+                )}
+                selected={startDate}
+                onChange={(date) => setStartDate(date)}
+                monthsShown={2}
+            />
+        </div>
+    )
+}
--- front-nido/src/components/Recomendations/index.jsx
+++ front-nido/src/components/Recomendations/index.jsx
@@ -1,16 +1,18 @@
 import cards from './cards.json';
 import Card from "./Card";
 import './styles/Cards.css';
-
-function Recomendations(){
-    return(
+import Calendar from "../CalendarReserve/index";
+function Recomendations() {
+    return (
         <>
-        <div className="cards">
-        {
-            cards.map(card => {
-            return(<Card key={card.id} card={card}/>)
-        })}
-        </div>
+            <div className="cards">
+                {
+                    cards.map(card => {
+                        return (<Card key={card.id} card={card} />)
+                    })}
+                <Calendar />
+            </div>
+
         </>
     )
 }
