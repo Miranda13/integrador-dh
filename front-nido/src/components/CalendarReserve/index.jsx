b9a319ec07482cb79a4c6f078b8ad07927cd9c3e
Jhonathan Antonio Colque <jantoniocolque@outlook.com>
2021-11-08 13:27:56 -0300

    add component calendar reserves

--- front-nido/src/components/CalendarReserve/index.jsx
+++ front-nido/src/components/CalendarReserve/index.jsx
@@ -4,8 +4,9 @@ import "react-datepicker/dist/react-datepicker.css";
 import { registerLocale, setDefaultLocale } from "react-datepicker";
 import "./CalendarReserve.css";
 import es from 'date-fns/locale/es';
-export default function CalendarReserve({ arrayDates }) {
+export default function CalendarReserve() {
     registerLocale('es', es)
+    const arrayDates = ["2021-11-11", "2021-12-05", "2021-11-13", "2021-11-14", "2021-11-20", "2021-12-15", "2021-12-17", "2021-12-20"];
     const [startDate, setStartDate] = useState(null);
     const [endDate, setEndDate] = useState();
     function editNamesDaysWeek() {
@@ -14,10 +15,16 @@ export default function CalendarReserve({ arrayDates }) {
             name.innerHTML = name.textContent.substring(0, 1).toUpperCase();
         })
     }
-    const isWeekday = (date) => {
-        const day = date.getDay();
-        return day !== 0 && day !== 6;
-    };
+    function convertFormatArrayDatesForExcludeCalendar() {
+        const arrayDatesFormat = [];
+        arrayDates.forEach(date => {
+            const dateFormat = new Date(date.split("-")[0], date.split("-")[1] - 1, date.split("-")[2]);
+            arrayDatesFormat.push(dateFormat);
+        })
+        return arrayDatesFormat;
+    }
+    useEffect(() => {
+    }, [])
     useEffect(() => {
         editNamesDaysWeek();
     }, [startDate, endDate])
@@ -27,7 +34,7 @@ export default function CalendarReserve({ arrayDates }) {
                 inline
                 locale="es"
                 minDate={new Date()}
-                filterDate={isWeekday}
+                excludeDates={convertFormatArrayDatesForExcludeCalendar()}
                 renderCustomHeader={({
                     monthDate,
                     customHeaderCount,
--- front-nido/src/components/Recomendations/index.jsx
+++ front-nido/src/components/Recomendations/index.jsx
@@ -1,7 +1,6 @@
 import cards from './cards.json';
 import Card from "./Card";
 import './styles/Cards.css';
-import Calendar from "../CalendarReserve/index";
 function Recomendations() {
     return (
         <>
@@ -10,9 +9,7 @@ function Recomendations() {
                     cards.map(card => {
                         return (<Card key={card.id} card={card} />)
                     })}
-                <Calendar arrayDates={["2020-11-07", "2020-11-10"]} />
             </div>
-
         </>
     )
 }
