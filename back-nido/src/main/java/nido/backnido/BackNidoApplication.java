2c57efd494aa17194555fc7f373c45925b4a96d7
Bruno Pugliese <bpug13@gmail.com>
2021-11-11 12:39:52 -0300

    Detalles refinados

--- back-nido/src/main/java/nido/backnido/BackNidoApplication.java
+++ back-nido/src/main/java/nido/backnido/BackNidoApplication.java
@@ -3,9 +3,6 @@ package nido.backnido;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
-
-@EnableEncryptableProperties
 @SpringBootApplication
 public class BackNidoApplication {
 
