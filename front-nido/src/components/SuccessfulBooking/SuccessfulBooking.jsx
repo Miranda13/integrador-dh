1a4ad853bac91f8a2af9f638a7f8186647b087f2
sofi-gonzalez <sofi.agz85@gmail.com>
2021-11-24 15:43:23 -0300

    Front - ajustes csss

--- front-nido/src/components/SuccessfulBooking/SuccessfulBooking.jsx
+++ front-nido/src/components/SuccessfulBooking/SuccessfulBooking.jsx
@@ -6,7 +6,7 @@ export default function SuccessfulBooking  (){
             <i className="successful-booking-icon fas fa-check-circle"></i>
             <h2>¡Muchas gracias!</h2>
             <p>Su reserva se ha realizado con éxito</p>
-            <button className="successful-booking-button button-1">ok</button>
+            <button className="successful-booking-button button-1">Ok</button>
         </div>
     )
 }
