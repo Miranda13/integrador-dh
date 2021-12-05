2f99b1c759041bf3b620949434c2f9821f65e827
Jhonathan Antonio Colque <jantoniocolque@outlook.com>
2021-12-05 10:18:55 -0300

    public route login and signin redirect a home

--- front-nido/src/routes/PublicRoute.jsx
+++ front-nido/src/routes/PublicRoute.jsx
@@ -2,7 +2,8 @@ import { Route, Navigate, Outlet } from 'react-router-dom';
 import { useContext } from 'react';
 import SessionContext from '../context/sessionContext';
 export default function PrivateRoute() {
-    const { user } = useContext(SessionContext);
+    // const { user } = useContext(SessionContext);
+    const user = localStorage.getItem('user');
     return (
         <>
             {
